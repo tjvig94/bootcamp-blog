8e21e08379a84f64a9ccdd10ec8f8d9c113af1c9
Tim Vigneau <tjvig94@gmail.com>
2021-04-04 21:35:04 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false, alter: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'))
+}).catch(err => console.log(err));
